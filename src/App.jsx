1448380f6d74d86aa050e6606ffa4f88ca0abbe0
Mehedi Hasan Murad <mehedihasna500422@gamil.com>
2024-07-27 23:58:56 +0600

    another component added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Courses from './components/Courses/Courses'
+
+function App() {
+
+
+  return (
+    <div className='font-poppins mt-12 mb-12'>
+      <h1 className='text-4xl text-center'>Courses Registration</h1>
+
+      <Courses></Courses>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Courses/Courses.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+const Courses = () => {
+    const [courses,setCourses] = useState([])
+    useEffect(() =>{
+        fetch("courses.json")
+        .then(res => res.json())
+        .then(data => setCourses(data))
+    },[])
+    return (
+        <div>
+           <h1 className='text-center'>Courses : {courses.length}</h1> 
+
+           {
+            
+           }
+        </div>
+    );
+};
+
+export default Courses;
